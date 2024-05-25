1d9dfa2e3bba6ba1b536c5d7fa80b8c1995d2983
Mleeah <102471691+Mleeah@users.noreply.github.com>
2024-05-25 11:54:51 +0300

    added Remove() to LinkedList class

--- LinkedList.py
+++ LinkedList.py
@@ -1,3 +1,4 @@
+
 class Node:
     def __init__(self, value):
         self.value = value
@@ -15,17 +16,6 @@ class LinkedList:
             currentNext = currentNext.next
         currentNext.next = newNode
 
-    def remove():
-        pass   
-
-    def length(self):
-        count = 0
-        currentNext = self.head
-        while currentNext.next is not None:
-            count+=1
-            currentNext = currentNext.next
-        return count+1
-    
     def insert(self, value, pos):
         newNode = Node(value)
 
@@ -47,6 +37,38 @@ class LinkedList:
         newNode.next = currentNext.next
         currentNext.next = newNode
 
+    def remove(self, pos):
+        if pos == 0:
+            self.head = self.head.next
+            return
+        
+        count = 0
+        currentNext = self.head
+        previous = self.head
+        while count < pos-1:
+            if currentNext.next is None:
+                print("Insertion Out of Bounds")
+                return
+            count+=1
+            previous = currentNext
+            currentNext = currentNext.next
+        
+        previous = currentNext
+        currentNext = currentNext.next
+        previous.next = currentNext.next
+
+        
+
+    def length(self):
+        count = 0
+        currentNext = self.head
+        while currentNext.next is not None:
+            count+=1
+            currentNext = currentNext.next
+        return count+1
+    
+    
+
 
     def printList(self):
         currentNext = self.head
@@ -64,5 +86,7 @@ root.append(5)
 
 root.append(15)
 root.insert(2, 4)
+root.printList()
+root.remove(4)
 print("length is ", root.length())
 root.printList()
