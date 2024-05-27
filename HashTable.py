53e333765f5ec9ad5770d689f37497c99e9e5f88
Mleeah <102471691+Mleeah@users.noreply.github.com>
2024-05-27 22:42:48 +0300

    Started working to HashTable, wrote probable function names

---
+++ HashTable.py
@@ -0,0 +1,24 @@
+if True:
+    DICTIONARY_SIZE = 26
+    NUMBER = 13
+
+class HashTable:
+    def __init__(self):
+        self.list = [] * DICTIONARY_SIZE
+
+    def hashFunc(key):
+        return (key * NUMBER) % DICTIONARY_SIZE
+
+    def insert(self, key, value):
+        pass
+
+    def find(self, key, value):
+        pass
+
+    def remove(self, key, value):
+        pass
+
+
+
+if True:
+    pass
